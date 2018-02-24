191fee39525c06722aa4b444378a10b637c915ff
Marco Oderkerk <mia@oderkerk.de>
2018-02-24 22:08:19 +0100

    Project init

---
+++ src/main/java/de/oderkerk/learning/physicslib/App.java
@@ -0,0 +1,13 @@
+package de.oderkerk.learning.physicslib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
