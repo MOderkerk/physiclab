4e5471f5c1099eff6b76047bc3a7e9ba89875ba5
Marco Oderkerk <mia@oderkerk.de>
2018-03-03 08:54:17 +0100

    gist.sh updated

--- gist.sh
+++ gist.sh
@@ -1,20 +1,14 @@
 #!/bin/bash -e
 
-: "${BRANCHES_TO_MERGE_REGEX?}" "${BRANCH_TO_MERGE_INTO?}"
-: "${GITHUB_SECRET_TOKEN?}" "${GITHUB_REPO?}"
+
 
 export GIT_COMMITTER_EMAIL='github@oderkerk.de'
 export GIT_COMMITTER_NAME='automated deploy'
 
-if ! grep -q "$BRANCHES_TO_MERGE_REGEX" <<< "$TRAVIS_BRANCH"; then
-    printf "Current branch %s doesn't match regex %s, exiting\\n" \
-        "$TRAVIS_BRANCH" "$BRANCHES_TO_MERGE_REGEX" >&2
-    exit 0
-fi
 
 # Since Travis does a partial checkout, we need to get the whole thing
 repo_temp=$(mktemp -d)
-git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
+git clone "https://github.com/$TRAVIS_REPO_SLUG" "$repo_temp"
 
 # shellcheck disable=SC2164
 cd "$repo_temp"
@@ -27,7 +21,7 @@ git merge --ff-only "$TRAVIS_COMMIT"
 
 printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
 
-push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
+push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_SECRET_TOKEN"
 
 # Redirect to /dev/null to avoid secret leakage
 git push "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
